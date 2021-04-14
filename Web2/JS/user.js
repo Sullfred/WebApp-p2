f8519d00414b4a6a7d6db2b0a10546a9731e2743
ironmand123 <74530153+ironmand123@users.noreply.github.com>
2021-04-14 12:25:12 +0200

    level (hardcoded)

--- Web2/JS/user.js
+++ Web2/JS/user.js
@@ -1,10 +1,18 @@
 //const {Name} = require('../json/user.json');
 
 let Name = "Jens Jensen";
+let level = "69";
 addname(Name);
+addlevel(level);
 
 function addname(Name) {
     let helloUser = document.getElementById('helloUser');
     helloUser.innerHTML += ` ${Name}`;
     
+}
+
+
+function addlevel(level){
+    let userlevel = document.getElementById('level');
+    userlevel.innerHTML += ` ${level}`;
 }
