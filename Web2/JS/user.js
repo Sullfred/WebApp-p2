00269626456a36a0fa709a5d05b3eaf53ec7b31a
Sullfred <74530142+Sullfred@users.noreply.github.com>
2021-04-14 11:26:40 +0200

    js added

---
+++ Web2/JS/user.js
@@ -0,0 +1,8 @@
+//const {Name} = require('../json/user.json');
+
+let userText = addname;
+
+function addname(name) {
+    let helloUser = document.querySelector("h3.character");
+    console.log(helloUser)
+}
