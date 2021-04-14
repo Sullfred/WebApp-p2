cd31b86eea31c180a7663492792bf9cc7b5f37aa
Run396 <Run396@users.noreply.github.com>
2021-04-14 22:10:02 +0200

    Ok

--- Web2/JS/lærer.js
+++ Web2/JS/lærer.js
@@ -7,21 +7,9 @@ function klasse(a) {
 
 let tester = ["A.1", "B.2", "C.3", "D.4"];
 
-/*
-function tilfoj() {
-    let html =
-        tester.map(function (tester) {
-                return '<li>' +'<a onclick=\"klasse(tester)\">' + tester + '</a>' + '</li>';
-            }).join('');
-    document.getElementById('list').innerHTML = html;
-}
-*/
-
 function tilfoj() {
     tester.forEach(i => {    
         let completelist= document.getElementById("list");        
-        completelist.innerHTML += "<li><a >" + i + "</a></li>";
+        completelist.innerHTML += "<li><a onclick=klasse('" + i + "')>" + i + "</a></li>";
     });
 }
-
-// prov med id/navn masske bitch;
