4364068301a67d44fc066a74385297a34d58e665
Sullfred <74530142+Sullfred@users.noreply.github.com>
2021-04-14 12:33:27 +0200

    homework function added

--- Web2/JS/user.js
+++ Web2/JS/user.js
@@ -2,17 +2,29 @@
 
 let Name = "Jens Jensen";
 let level = "69";
+let lektier = 1;
+
 addname(Name);
 addlevel(level);
+addHomework(lektier);
 
 function addname(Name) {
     let helloUser = document.getElementById('helloUser');
     helloUser.innerHTML += ` ${Name}`;
-    
 }
 
 
 function addlevel(level){
     let userlevel = document.getElementById('level');
     userlevel.innerHTML += ` ${level}`;
+}
+
+function addHomework(lektier) {
+    let homework = document.getElementById('lektier');
+
+    if (lektier)
+        homework.innerHTML = "Du har lektier for";
+    else
+    homework.innerHTML = "Du har ikke lektier for";
+
 }
