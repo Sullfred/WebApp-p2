078dd66e4d0ec71992dfc59a8d83f8172b3628b4
Run396 <Run396@users.noreply.github.com>
2021-04-16 10:17:54 +0200

    Nice

--- Web2/JS/lærer.js
+++ Web2/JS/lærer.js
@@ -1,15 +1,22 @@
 function klasse(a) {
-    let h1 = document.createElement("H1");
-    let tekst = document.createTextNode(a);
-    h1.appendChild(tekst);
-    document.getElementById("skovl").appendChild(h1);
+    document.getElementById("klassevalg").innerHTML = "Klasse: " + a;
+    document.getElementById("elevlist").innerHTML = '';
+    elever();
 }
 
-let tester = ["A.1", "B.2", "C.3", "D.4"];
+let klasseliste = ["1.A", "2.B", "3.C", "4.D"];
+let elevliste = ["DJonny", "Jimmi", "Brian", "Din mor"];
 
 function tilfoj() {
-    tester.forEach(i => {    
-        let completelist= document.getElementById("list");        
+    klasseliste.forEach(i => {    
+        let completelist= document.getElementById("klasselist");        
         completelist.innerHTML += "<li><a onclick=klasse('" + i + "')>" + i + "</a></li>";
+    });
+}
+
+function elever(){
+    elevliste.forEach(i => {
+    let completelist= document.getElementById("elevlist");        
+    completelist.innerHTML += "<li><a href=" + "https://www.youtube.com/watch?v=dQw4w9WgXcQ>" + i + "</a></li>";
     });
 }
