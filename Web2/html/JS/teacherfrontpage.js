2b1704846982afc27730ae0c0ae8f58eae904296
NightOwl1998 <nreece20@student.aau.dk>
2021-04-21 14:34:03 +0200

    update

---
+++ Web2/html/JS/teacherfrontpage.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let user=["Egon Olsen"];
+function navuser() {
+    let list = document.getElementById("user");
+    list.innerHTML += "<li><a class='username'>"+user[0]+"</li></a>";
+}
