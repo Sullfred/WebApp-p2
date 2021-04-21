4892420a2a7cc89747d4ff61984acab476ab2b5b
Sullfred <74530142+Sullfred@users.noreply.github.com>
2021-04-21 13:03:32 +0200

    Update user.js

--- Web2/html/JS/user.js
+++ Web2/html/JS/user.js
@@ -45,27 +45,6 @@ function solvedAssignments(assignmentType){
     solved.innerHTML++
 }
 
-function leveling(currentXp, currentLevel, earnedXp, requiredXp){
-    let newXp = currentXp + earnedXp
-    if (newXp >= requiredXp){
-        currentLevel++;
-        currentXp = newXp-requiredXp;
-        requiredXp = Math.floor(requiredXp*1.2);
-        if(currentXp >= requiredXp){
-            console.log(`CurrentXp:${currentXp}, Currentlvl:${currentLevel}, NewXp:${newXp}, RequiredXp:${requiredXp}`);
-            leveling(currentXp, currentLevel, 0, requiredXp);
-        }
-        else{
-            console.log(`CurrentXp:${currentXp}, Currentlvl:${currentLevel}, NewXp:${newXp}, RequiredXp:${requiredXp}`);
-            return currentLevel, currentXp, requiredXp;
-        }
-    }
-    else{
-        console.log(`CurrentXp:${currentXp}, Currentlvl:${currentLevel}, NewXp:${newXp}, RequiredXp:${requiredXp}`)
-        return newXp;
-    }
-}
-
 function xpView(currentXp, requiredXp){
     xpObject = document.querySelector('#xp')
     xpObject.innerHTML = `${currentXp}/${requiredXp}`
