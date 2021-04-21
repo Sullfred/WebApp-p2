76fb64b2a4119c0e929198844f17a0456532ab65
Sullfred <74530142+Sullfred@users.noreply.github.com>
2021-04-21 13:22:29 +0200

    Update homework.js

--- Web2/html/JS/homework.js
+++ Web2/html/JS/homework.js
@@ -2,7 +2,15 @@
 
 const homeworkSite = document.querySelector('#gotHomework')
 
-const header = document.createElement('h1');
-const headerText = document.createTextNode('Shalom');
+const mathSubjects = document.createElement('h1');
+const subjectText1 = document.createTextNode('Plus');
+const subjectText2 = document.createTextNode('Minus');
+const subjectText3 = document.createTextNode('Gange');
+const subjectText4 = document.createTextNode('Dividere');
+const subjectText5 = document.createTextNode('Blandet');
 
-homeworkSite.appendChild(header.appendChild(headerText));
+homeworkSite.appendChild(mathSubjects.appendChild(subjectText1));
+homeworkSite.appendChild(mathSubjects.appendChild(subjectText2));
+homeworkSite.appendChild(mathSubjects.appendChild(subjectText3));
+homeworkSite.appendChild(mathSubjects.appendChild(subjectText4));
+homeworkSite.appendChild(mathSubjects.appendChild(subjectText5));
