cacc4091e91a63594573630cec3f31a1c9cf4ff4
Run396 <Run396@users.noreply.github.com>
2021-04-21 10:33:17 +0200

    Translated (oversat)

---
+++ Web2/html/JS/teacher-page.js
@@ -0,0 +1,27 @@
+let classes = ["1.A", "2.B", "3.C", "4.D", "5.E"];
+let students = ["Esben.N", "Rune.F", "Jimmi", "Brian", "Nathan.R"];
+
+function headerclass(a) {
+    document.getElementById("classchoice").innerHTML ="Klasse: " + a;
+    document.getElementById("studentlist").innerHTML = '';
+    elever();
+}
+
+function studentinfo(a) {
+    document.getElementById("studentinformation").innerHTML = "Elev: " + a;
+    document.getElementById("studentinformation").innerHTML = "Elev: " + a;
+}
+
+function addclass() {
+    classes.forEach(i => {    
+        let completelist= document.getElementById("classlist");        
+        completelist.innerHTML += "<li><a onclick=headerclass('" + i + "')>" + i + "</a></li>";
+    });
+}
+
+function elever(){
+    students.forEach(i => {
+        let completelist= document.getElementById("studentlist");        
+        completelist.innerHTML += "<li><a onclick=studentinfo('" + i + "')>" + i + "</a></li>";
+    });
+}
