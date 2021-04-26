226a49e429d625019732c5844ec2267e1ab3bcb8
Sullfred <74530142+Sullfred@users.noreply.github.com>
2021-04-26 08:57:33 +0200

    hide/show things

--- Web2/html/JS/homework.js
+++ Web2/html/JS/homework.js
@@ -1,16 +1,15 @@
 
+let user = {
+    Name: "Jens Jensen",
+    currentLevel: 0,
+    lektier: 1,
+    currentXp: 0,
+    requiredXp: 15,
+};
 
 const homeworkSite = document.querySelector('#gotHomework')
 
-const mathSubjects = document.createElement('h1');
-const subjectText1 = document.createTextNode('Plus');
-const subjectText2 = document.createTextNode('Minus');
-const subjectText3 = document.createTextNode('Gange');
-const subjectText4 = document.createTextNode('Dividere');
-const subjectText5 = document.createTextNode('Blandet');
-
-homeworkSite.appendChild(mathSubjects.appendChild(subjectText1));
-homeworkSite.appendChild(mathSubjects.appendChild(subjectText2));
-homeworkSite.appendChild(mathSubjects.appendChild(subjectText3));
-homeworkSite.appendChild(mathSubjects.appendChild(subjectText4));
-homeworkSite.appendChild(mathSubjects.appendChild(subjectText5));
+if (user.lektier)
+    homeworkSite.style.display = "block";
+else
+    homeworkSite.style.display = "none";
