6a735bb960b9a1805b66462a6ddc918c00c2ac4a
Sullfred <daniel.v.kleist@gmail.com>
2021-04-28 11:28:20 +0200

    Update table.js

--- Web2/html/JS/table.js
+++ Web2/html/JS/table.js
@@ -88,10 +88,10 @@ function createUserData(){
 
 }
 function randClass(){
-    let classroom = Math.round(Math.random()*3)
+    let classroom = Math.floor(Math.random()*3)
     let suffix = ["a", "b", "c"]
     if(classroom != 0){
-        return classroom + "." + `${suffix[Math.round(Math.random()*3)]}`
+        return classroom + "." + `${suffix[Math.floor(Math.random()*3)]}`
     }
     else
     randClass()
@@ -99,8 +99,8 @@ function randClass(){
 function randName(){
     let firstNames = ["Nigel", "Clement", "Jamal", "Bert", "Olen", "Noel", "Emerson", "Leo", "Kelvin", "Albert", "Harland", "Jerrod", "Kevin","Brain", "Tim", "Isaiah", "Augustine", "Grover", "Beau", "Tyrone", "Casie", "Kizzie", "Joann", "Charmain", "Kiara", "Beckie", "Ammiez", "Thu", "Vernell", "Lurlene", "Katelyn", "Johna", "Christeen", "Darleen", "Cathrine", "Shavonne", "Yevette", "Myrta", "Collette", "Roxane"]
     let lastNames = ["Petty", "Webster", "Campbell", "Gilbert", "Benjamin", "Turner", "Gates", "Alvarado", "Murphy", "Chung", "Byrd", "Schneider", "Aguilar", "Stanton", "Burns", "Norton", "Macdonald", "Hancock", "Conrad", "Ramos", "Gill", "Savage", "Mccann", "Montes", "French", "Figueroa", "Guerra", "Mayo", "Nichols", "Zamora", "Meyer", "Mathis", "James", "Terrell", "Graham", "Hickman", "Paul", "Mcpherson", "Pineda", "Novak"]
-        let randomFirstName = firstNames[Math.round(Math.random()*39)]
-        let randomLastName = lastNames[Math.round(Math.random()*39)]
+        let randomFirstName = firstNames[Math.floor(Math.random()*39)]
+        let randomLastName = lastNames[Math.floor(Math.random()*39)]
     return randomFirstName + " " + randomLastName;
 }
 
