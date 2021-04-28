6a21229f53f505aaccc93a3448f1f281a396cb94
Benjamin Porsmose Pedersen <bpeder19@student.aau.dk>
2021-04-28 11:04:39 +0200

    Update table.js

--- Web2/html/JS/table.js
+++ Web2/html/JS/table.js
@@ -71,7 +71,6 @@ function createUserData(){
                 CurrentXp INT DEFAULT "0",
                 RequiredXp INT DEFAULT "15",
                 Homework BOOL DEFAULT "1",
-                FOREIGN KEY (UserLoginId) REFERENCES Users(UserLoginId)
             )`
     con.query(sql, function (err, result) {
         if (err) throw err;
