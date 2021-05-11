af567825ceacb3a8e42b14eb24fe787d6da8646f
Sullfred <74530142+Sullfred@users.noreply.github.com>
2021-05-11 11:02:45 +0200

    Update student.js

--- Web2/routes/student.js
+++ Web2/routes/student.js
@@ -4,6 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('../public/student.html');
+
+  const id = req.query.id
+  console.log(id);
 });
 
 module.exports = router;
