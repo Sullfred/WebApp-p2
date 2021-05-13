063361069765642d634c12f3ab1f26bd7d374b2a
snotskovl <enorvi20@student.aau.dk>
2021-05-13 11:43:14 +0200

    rune er grim

--- Web2/html/JS/homeworkcreator.js
+++ Web2/html/JS/homeworkcreator.js
@@ -3,10 +3,12 @@ function reset() {
   document.getElementById("task").reset();
 }
 
+
+
+
 function saveopg() {
-    const opgave=document.getElementById("task");
-    alert(opgave.value);
 }
+
 
 function addop(a) {
     document.getElementById("task").value += a;
