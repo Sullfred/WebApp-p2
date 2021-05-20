6f78a31a548ee99d4f902c507ccd4789b1a46b85
Sullfred <74530142+Sullfred@users.noreply.github.com>
2021-05-20 14:08:18 +0200

    test

--- Web2/public/javascripts/leveling.js
+++ Web2/public/javascripts/leveling.js
@@ -1,23 +1,24 @@
-export function leveling(currentXp, currentLevel, earnedXp, requiredXp);
+//export function leveling(currentXp, currentLevel, earnedXp, requiredXp);
 
-function leveling(currentXp, currentLevel, earnedXp, requiredXp){
+exports.leveling = (currentXp, currentLevel, earnedXp, requiredXp) => {
     let newXp = currentXp + earnedXp
     if (newXp >= requiredXp){
         currentLevel++;
         currentXp = newXp-requiredXp;
         requiredXp = Math.floor(requiredXp*1.2);
         if(currentXp >= requiredXp){
-            console.log(`CurrentXp:${currentXp}, Currentlvl:${currentLevel}, NewXp:${newXp}, RequiredXp:${requiredXp}`);
+            //console.log(`CurrentXp:${currentXp}, Currentlvl:${currentLevel}, NewXp:${newXp}, RequiredXp:${requiredXp}`);
             leveling(currentXp, currentLevel, 0, requiredXp);
         }
         else{
-            console.log(`CurrentXp:${currentXp}, Currentlvl:${currentLevel}, NewXp:${newXp}, RequiredXp:${requiredXp}`);
-            return currentLevel, currentXp, requiredXp;
+            //console.log(`CurrentXp:${currentXp}, Currentlvl:${currentLevel}, NewXp:${newXp}, RequiredXp:${requiredXp}`);
+            return [currentLevel, currentXp, requiredXp];
         }
     }
     else{
-        console.log(`CurrentXp:${currentXp}, Currentlvl:${currentLevel}, NewXp:${newXp}, RequiredXp:${requiredXp}`)
-        return newXp;
+        //console.log(`CurrentXp:${currentXp}, Currentlvl:${currentLevel}, NewXp:${newXp}, RequiredXp:${requiredXp}`)
+        let currentXp = newXp
+        return [currentLevel, currentXp, requiredXp];
     }
 
 }
--- unit.test.js
+++ unit.test.js
@@ -1,7 +1,13 @@
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
 const { testfunction } = require('./unit');
+const {leveling} = require('./Web2/public/javascripts/leveling')
 
 test('Test of testfunction', () => {
     text = testfunction("test1")
     expect(text).toBe('this test will retrun the parameter name: test1')
 });
+
+test('Test if level up function is working', () => {
+    numberArray1= leveling(0,1,15,15);
+    expect(numberArray).toBe(1,0,18)
+})
