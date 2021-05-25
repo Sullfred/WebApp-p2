af46dd13ae20e12563a9a208b6e7f3bfa2ee45e6
Benjamin Porsmose Pedersen <bpeder19@student.aau.dk>
2021-05-25 11:40:36 +0200

    .....

--- Web2/public/javascripts/login.js
+++ Web2/public/javascripts/login.js
@@ -18,9 +18,13 @@ function postLogin(){
     function onLoad() {
         var response = this.responseText;
         var parsedResponse = JSON.parse(response);
+<<<<<<< Updated upstream
         if(parsedResponse[0] === "Elev")
             window.location.replace(window.location.href.replace("login", "student"))
         else if(parsedResponse[0] === "Lærer")
+=======
+        if(parsedResponse[0] === "Lærer"){
+>>>>>>> Stashed changes
             window.location.replace(window.location.href.replace("login", "teacher"))
         else
             window.alert("Error on login")
